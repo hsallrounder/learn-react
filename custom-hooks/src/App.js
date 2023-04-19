01d08b67a1ed98aa4079be6212e35a527f8a7c78
hsallrounder <hsallrounder@gmail.com>
2023-04-19 15:04:28 +0530

    customHooks

---
+++ custom-hooks/src/App.js
@@ -0,0 +1,24 @@
+import React, { useState } from "react"
+import './App.css';
+import Section from "./components/Section";
+import useToggle from "./components/hooks/Toggle";
+
+function App() {
+  const [smileyface, isSmileyFace] = useState(true);
+
+  const [section, updateSection] = useToggle(true);
+
+  return (
+    <div className="App">
+
+      {section && <Section />}
+
+      <p style={{ textAlign: "center" }}><button onClick={()=>updateSection()}>Read More</button></p>
+
+      <h1 style={{ textAlign: "center" }} onClick={() => isSmileyFace(!smileyface)}> {(smileyface) ? "😂" : "🦍"} </h1>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ custom-hooks/src/components/Section.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Section = () => {
+    return (
+        <div>
+            <p style={{ width: "60vw", backgroundColor: "aqua", border: "2px solid black", margin: "50px auto" }}>
+
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis, consequuntur dignissimos debitis ipsum temporibus possimus odit! Repellendus iure quasi qui a harum, amet illum placeat earum velit fugit in praesentium?
+                Voluptates laudantium suscipit, necessitatibus nulla repellat esse impedit eaque, repudiandae quasi soluta eum ut quidem. Incidunt at quam minima perferendis hic veniam, voluptates doloremque placeat corporis? Maxime, nisi? Dicta, commodi.
+                Soluta alias consectetur quidem aspernatur perspiciatis, in quam! Eligendi quasi vero eveniet eum deserunt ipsum magni assumenda, laudantium, repellendus id consequatur velit quia numquam a perspiciatis fuga sapiente fugiat mollitia?
+                Provident voluptate labore facere aspernatur et nam, perspiciatis, explicabo unde quis adipisci quibusdam pariatur voluptatem deserunt porro ab quos consectetur sit expedita iure quasi amet deleniti necessitatibus. Magni, itaque facilis?
+                Minima consequuntur eveniet doloremque aut ratione, totam, numquam tenetur, dolor perspiciatis corporis officiis debitis inventore in voluptates iusto nesciunt. Animi corrupti provident laudantium officiis voluptas labore fuga repellendus modi perspiciatis.
+
+            </p>
+        </div>
+    )
+}
+export default Section
